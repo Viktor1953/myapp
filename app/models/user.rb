d3997a0f19df9a3fc9cf48a3127f42770ba9cceb
Viktor Skoromnik <viktor.skoromnik@gmail.com>
2024-01-05 12:59:49 +0300

    Add turbo User, Room, min design Rooms

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   
   validates_uniqueness_of :username
   scope :all_except, ->(user) { where.not(id: user) }
+  after_create_commit { broadcast_append_to "users" }
+
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
